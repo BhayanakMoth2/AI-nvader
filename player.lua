bd2b0856079ffc3cc810f223453fba1c01845648
BhayanakMoth2 <33507550+BhayanakMoth2@users.noreply.github.com>
2018-04-07 20:37:03 +0530

    Display keeps track of enemy and player pulse

--- player.lua
+++ player.lua
@@ -17,29 +17,39 @@ boxEdge = 100
 nesHeight = 240
 nesWidth = 255
 pulsePos = {}
+enemyPulse = {}
+enemyPulsePos = {}
 -------------Getters--------------------
-function getPosition()
-    x =  mainmemory.readbyte(0x070F);
-    
+function checkEnemyPulse()
+    enemyPulse[0] = mainmemory.readbyte(0x0716)
+    enemyPulse[1] = mainmemory.readbyte(0x071A)
+    enemyPulse[2] = mainmemory.readbyte(0x071E)
+    enemyPulse[3] = mainmemory.readbyte(0x0722)
+    enemyPulse[4] = mainmemory.readbyte(0x0726)
 end
-function isPulseDead()
+function getEnemyPulsePos(i)
+    local pos = {}
+    pos[0] = mainmemory.readbyte(0x0717 + i*4)
+    pos[1] = mainmemory.readbyte(0x0714 + i*4)
+    return pos
+end
+function checkPulse()
    pulseTrig = mainmemory.readbyte(0x0712);
    return pulseTrig
 end
-function GetPulsePos()
+function getPulsePos()
     pulseX = mainmemory.readbyte(0x0713)
     pulseY = mainmemory.readbyte(0x0710)
-    pulsePos[0] = math.floor(pulseX)
-    pulsePos[1] = math.floor(pulseY)
-    return pos
+    pulsePos[0] = pulseX
+    pulsePos[1] = pulseY
 end
-function GetPlayerX()
+function getPlayerX()
     playerX = mainmemory.readbyte(0x070F)
-    return math.floor(playerX)
+    return playerX
 end
 
-function GetPlayerY()
-    return math.floor(mainmemory.readbyte(0x070C))
+function getPlayerY()
+    return mainmemory.readbyte(0x070C)
 end
 --------------Getters--------------------
 
@@ -57,13 +67,21 @@ end
 
 function LoadCells()
     clearCells()
-    if(isPulseDead() == 0) then
-        pos = GetPulsePos()
+    if(checkPulse() == 0) then
+        pos = getPulsePos()
         index = toCellX(pulsePos[0])+toCellY(pulsePos[1])*boxEdge
         cells[index] = 3
     end
-    x = toCellX(GetPlayerX())
-    y = toCellY(GetPlayerY())
+    checkEnemyPulse()
+    for i = 0, 4 do
+        if(enemyPulse[i]==0) then
+            pos = getEnemyPulsePos(i)
+            index = toCellX(pos[0])+toCellY(pos[1])*boxEdge
+            cells[index] = 4
+        end
+    end
+    x = toCellX(getPlayerX())
+    y = toCellY(getPlayerY())
     cells[y*boxEdge+x] = 1
     
 end
@@ -79,17 +97,22 @@ end
 
 function WillDrawShit()
      gui.drawRectangle(0,0,100,100,0x33FFFFFF,0x33FFFFFF)
+     local val = 0
      for i = 1, boxEdge do
         for j = 1, boxEdge do 
-            if(cells[j*boxEdge + i]== 3) then
+            val = cells[j*boxEdge + i]
+            if(val == 3) then
                 gui.drawBox(i,j,i+1,j+1,red,red)
+            end
+            if(val == 4) then
+                gui.drawBox(i,j,i+1,j+1,green,green)
             end
         end
     end
 
 
-     x = toCellX(GetPlayerX())
-     y = toCellY(GetPlayerY())
+     x = toCellX(getPlayerX())
+     y = toCellY(getPlayerY())
      
      gui.drawBox(x,y,x+1,y+1,blue,blue)
 end
